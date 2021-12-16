5c74a39066177d556c760ae5acc34cc04fd5743b
piotr04j <darricck04@gmail.com>
2021-12-16 18:36:36 +0100

    added type and assertion in reucer

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,11 +1,12 @@
 import {damageType} from "../model/state";
 
-export const initial = () => ({type: "INITIAL"})
-export const verifyPlateNumber = (error: boolean) => ({type: "IDENTIFY_CAR", payload: { error }})
-export const chooseDamage = (plateNumber: number) => ({type: "CHOOSE_DAMAGE", payload: { plateNumber }})
+//TODO use thunk
+export const initial = () => ({type: "INITIAL" as const})
+export const verifyPlateNumber = (error: boolean) => ({type: "IDENTIFY_CAR" as const, payload: { error }})
+export const chooseDamage = (plateNumber: number) => ({type: "CHOOSE_DAMAGE" as const, payload: { plateNumber }})
 export const chooseContact = (plateNumber: number, error: boolean, chooseDamage: damageType) => {
     return {
-        type: "CONTACT_OPTION",
+        type: "CONTACT_OPTION" as const,
         payload: {
             plateNumber,
             error,
@@ -15,17 +16,17 @@ export const chooseContact = (plateNumber: number, error: boolean, chooseDamage:
 }
 export const callSeller = (plateNumber: number, error: boolean, chooseDamage: damageType) => {
     return {
-        type: "CALL_SELLER",
+        type: "CALL_SELLER" as const,
         payload: {
             plateNumber,
             error,
             chooseDamage
         }
-    }
+    }  as const
 }
 export const chatSeller = (plateNumber: number, error: boolean, chooseDamage: damageType) => {
     return {
-        type: "CHAT_SELLER",
+        type: "CHAT_SELLER" as const,
         payload: {
             plateNumber,
             error,
@@ -33,5 +34,13 @@ export const chatSeller = (plateNumber: number, error: boolean, chooseDamage: da
         }
     }
 }
-export const loading = () => ({type: 'LOADING'})
+export const loading = () => ({type: "LOADING" as const})
 
+export type Actions =
+    | ReturnType<typeof initial>
+    | ReturnType<typeof verifyPlateNumber>
+    | ReturnType<typeof chooseDamage>
+    | ReturnType<typeof chooseContact>
+    | ReturnType<typeof callSeller>
+    | ReturnType<typeof chatSeller>
+    | ReturnType<typeof loading>
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,8 +1,9 @@
 import {Reducer} from "redux";
 import {CarDamageAppState} from "../model/state";
+import {Actions} from "./actions";
+import {assertState} from "../model/assertState";
 
-// TODO adjust actions typing
-const reducer: Reducer = (state: CarDamageAppState = { type: "INITIAL"}, action: any) => {
+const reducer: Reducer = (state: CarDamageAppState = { type: "INITIAL"}, action: Actions) => {
     switch (action.type) {
         case "INITIAL":
             state = { type: "INITIAL"}
@@ -11,22 +12,27 @@ const reducer: Reducer = (state: CarDamageAppState = { type: "INITIAL"}, action:
             state = { type: "LOADING"}
             return state
         case "IDENTIFY_CAR":
+            assertState(state, "INITIAL", "IDENTIFY_CAR")
             state = { type: "IDENTIFY_CAR", error: action.payload.error }
             return state
         case "CHOOSE_DAMAGE":
-            state = { type: "CHOOSE_DAMAGE", plateNumber: action.payload.chooseDamage }
+            assertState(state,  "LOADING")
+            state = { type: "CHOOSE_DAMAGE", plateNumber: action.payload.plateNumber }
             return state
         case "CONTACT_OPTION":
+            assertState(state, "CHOOSE_DAMAGE", "CONTACT_OPTION")
             state = { type: "CONTACT_OPTION", error: action.payload.error, plateNumber: action.payload.plateNumber, chosenDamage: action.payload.chooseDamage }
             return state
         case "CALL_SELLER":
+            assertState(state, "LOADING")
             state = { type: "CALL_SELLER", plateNumber: action.payload.plateNumber, chosenDamage: action.payload.chooseDamage }
             return state
         case "CHAT_SELLER":
+            assertState(state, "LOADING")
             state = { type: "CHAT_SELLER",  plateNumber: action.payload.plateNumber, chosenDamage: action.payload.chooseDamage }
             return state
         default:
-            const leftover = state
+            const leftover = action
             return state
     }
 }
