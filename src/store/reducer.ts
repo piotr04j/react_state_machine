6b9df05868bf77d4c0b1af1d92f0d4fd9ea7e5ae
piotr04j <darricck04@gmail.com>
2021-12-16 19:24:43 +0100

    remove asserts function from reducer

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -3,7 +3,7 @@ import {CarDamageAppState} from "../model/state";
 import {Actions} from "./actions";
 import {assertState} from "../model/assertState";
 
-const reducer: Reducer = (state: CarDamageAppState = { type: "INITIAL"}, action: Actions) => {
+const reducer: Reducer = (state: CarDamageAppState = { type: "INITIAL"}, action) => {
     switch (action.type) {
         case "INITIAL":
             state = { type: "INITIAL"}
@@ -12,23 +12,18 @@ const reducer: Reducer = (state: CarDamageAppState = { type: "INITIAL"}, action:
             state = { type: "LOADING"}
             return state
         case "IDENTIFY_CAR":
-            assertState(state, "INITIAL", "IDENTIFY_CAR")
             state = { type: "IDENTIFY_CAR", error: action.payload.error }
             return state
         case "CHOOSE_DAMAGE":
-            assertState(state,  "LOADING")
             state = { type: "CHOOSE_DAMAGE", plateNumber: action.payload.plateNumber }
             return state
         case "CONTACT_OPTION":
-            assertState(state, "CHOOSE_DAMAGE", "CONTACT_OPTION")
             state = { type: "CONTACT_OPTION", error: action.payload.error, plateNumber: action.payload.plateNumber, chosenDamage: action.payload.chooseDamage }
             return state
         case "CALL_SELLER":
-            assertState(state, "LOADING")
             state = { type: "CALL_SELLER", plateNumber: action.payload.plateNumber, chosenDamage: action.payload.chooseDamage }
             return state
         case "CHAT_SELLER":
-            assertState(state, "LOADING")
             state = { type: "CHAT_SELLER",  plateNumber: action.payload.plateNumber, chosenDamage: action.payload.chooseDamage }
             return state
         default:
